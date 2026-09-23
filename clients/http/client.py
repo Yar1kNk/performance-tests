77a8e9d06308d27fb7ee09fb36f14ae176f82426
Yarik <yarrnkt@mail.ru>
2026-09-23 18:10:17 +0300

    event hooks

--- clients/http/client.py
+++ clients/http/client.py
@@ -1,5 +1,10 @@
 from httpx import Client, URL, QueryParams, Response
-from typing import Any
+from typing import Any, TypedDict
+
+
+class HTTPClientExtensions(TypedDict, total=False):
+    route: str
+
 
 class HTTPClient:
     """
@@ -11,17 +16,28 @@ class HTTPClient:
     def __init__(self, client: Client):
         self.client = client
 
-    def get(self, url: URL | str, params: QueryParams | None = None) -> Response:
+    def get(
+            self,
+            url: URL | str,
+            params: QueryParams | None = None,
+            extensions: HTTPClientExtensions | None = None
+    ) -> Response:
         """
         Выполняет GET-запрос.
 
         :param url: URL-адрес эндпоинта.
         :param params: GET-параметры запроса (например, ?key=value).
+        :param extentions: Дополнительные данные, передаваемые через HTTPX extensions
         :return: Объект Response с данными ответа.
         """
-        return self.client.get(url, params=params)
+        return self.client.get(url, params=params, extensions=extensions)
 
-    def post(self, url: URL | str, json: Any | None = None) -> Response:
+    def post(
+            self,
+            url: URL | str,
+            json: Any | None = None,
+            extensions: HTTPClientExtensions | None = None
+    ) -> Response:
         """
         Выполняет POST-запрос.
 
@@ -29,5 +45,5 @@ class HTTPClient:
         :param json: Данные в формате JSON.
         :return: Объект Response с данными ответа.
         """
-        return self.client.post(url, json=json)
+        return self.client.post(url, json=json, extensions=extensions)
 
